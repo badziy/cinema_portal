aabe480689de00fb1680dce7d4e3083c03db3046
Badziev Alan <badjity@yandex.ru>
2022-09-28 18:20:45 +0300

    Initial commit

---
+++ src/main/java/ru/badziy/cinema/portal/cinema_portal/CinemaPortalApplication.java
@@ -0,0 +1,13 @@
+package ru.badziy.cinema.portal.cinema_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemaPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/badziy/cinema/portal/cinema_portal/CinemaPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.badziy.cinema.portal.cinema_portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
